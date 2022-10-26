ea494ffbb16b1769ed08de3b81ecc533bb6f2254
Kevin Guan <20823498+guan-kevin@users.noreply.github.com>
2022-10-26 15:24:37 -0700

    Update index.js

--- .github/actions/test/index.js
+++ .github/actions/test/index.js
@@ -3,6 +3,7 @@ const input2 = process.env.INPUT_TEST_INPUT-2;
 const input3 = process.env.INPUT_TEST_INPUT-3;
 const input4 = process.env.INPUT_TEST_INPUT-4;
 console.log(`Input 1: ${input1}, Input 2: ${input2}, Input 3: ${input3}, Input 4: ${input4}`);
+console.log(process.env)
   
 console.log("Running within same directory")
 if (input1 != -1 || input2 != -2 || input3 != -3 || input4 != -4) {
