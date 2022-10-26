8e91fed9c441cc5bcfd99263e17f96c6e9c6495c
Kevin Guan <20823498+guan-kevin@users.noreply.github.com>
2022-10-26 15:21:54 -0700

    Update index.js

--- .github/actions/test/index.js
+++ .github/actions/test/index.js
@@ -1,6 +1,6 @@
 const input1 = process.env.TEST_INPUT-1;
 const input2 = process.env.TEST_INPUT-2;
-const input3 = process.env.TEST_INPUT-3);
+const input3 = process.env.TEST_INPUT-3;
 const input4 = process.env.TEST_INPUT-4;
 console.log(`Input 1: ${input1}, Input 2: ${input2}, Input 3: ${input3}, Input 4: ${input4}`);
   
