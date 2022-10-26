bc6500f9e781d4d0707220f79bd8281ebabf41ab
Kevin Guan <20823498+guan-kevin@users.noreply.github.com>
2022-10-26 15:20:28 -0700

    Create index.js

---
+++ .github/actions/test/index.js
@@ -0,0 +1,10 @@
+const input1 = process.env.TEST_INPUT-1;
+const input2 = process.env.TEST_INPUT-2;
+const input3 = process.env.TEST_INPUT-3);
+const input4 = process.env.TEST_INPUT-4;
+console.log(`Input 1: ${input1}, Input 2: ${input2}, Input 3: ${input3}, Input 4: ${input4}`);
+  
+console.log("Running within same directory")
+if (input1 != -1 || input2 != -2 || input3 != -3 || input4 != -4) {
+  x = y + 1;
+}
